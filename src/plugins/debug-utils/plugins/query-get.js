b85079b09d78c6bbe53581572a71d2b6ce08fd68
bb.slipers <bb.slipers@gmail.com>
2022-09-30 20:26:38 +0300

    Исправление в query-get

--- src/plugins/debug-utils/plugins/query-get.js
+++ src/plugins/debug-utils/plugins/query-get.js
@@ -1,22 +1,16 @@
-/**
- * Выполняет консольную команду в MineCraft
- */
+// Команда отладчика cmd-mc-get
+// Выводит на экран результат запроса
 
-
-function callback (res){
- 
-    const _res = JSON.parse(res)
-    _res.data = JSON.parse(_res.data)
-    const outputData = JSON.stringify(_res, null, 2);
-    vnjs.emit('vnjson.info', outputData)
+function callback (res) {
+	const outputType = vnjs.store.MINECRAFT.TYPE;
+	const outputData = JSON.stringify(vnjs.store.MINECRAFT[outputType], null, 2);
+    vnjs.emit('vnjson.info', outputData);
 }
 
 export default function (param) {
-    param.callback = callback.bind(this)
-    const plugins = { 
-              'mc-get': param 
-    }
-
-    this.exec(plugins)
-
+    param.callback = callback.bind(this);
+    const plugins = {
+		'mc-get': param 
+    };
+    this.exec(plugins);
 }
